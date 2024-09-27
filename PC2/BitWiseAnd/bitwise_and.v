06e8b98060fc4ae1bbe58121488937ec384d7b3b
davidluzhiyun <david.luzhiyun@gmail.com>
2024-09-26 21:06:32 -0400

    Bitwise and done

---
+++ PC2/BitWiseAnd/bitwise_and.v
@@ -0,0 +1,16 @@
+module bitwise_and (in1, in2, out);
+	// inputs definition
+	input [31:0] in1;
+	input [31:0] in2;
+	
+	// outputs definitio
+	output [31:0] out;
+	
+	genvar i;
+	
+	generate
+		for (i=1; i<=14; i=i+1) begin : name_for_generate_block//Due to Quartus Bug ID: 15010773232 we need to name the generate block
+			and(out[i], in1[i], in2[i]);
+		end
+	endgenerate
+endmodule
